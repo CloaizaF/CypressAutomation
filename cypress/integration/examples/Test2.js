1643196a8901afed5ef82b9265a51f5bc3c09966
CloaizaF <cloaiza@unal.edu.co>
2025-03-12 11:20:52 -0500

    Add test case to validate E2E scenario

---
+++ cypress/integration/examples/Test2.js
@@ -0,0 +1,20 @@
+/// <reference types="Cypress" />
+
+// cypress - Spec
+describe('My Second Test Suite', () => {
+    it('Search and Buy a Product', () => {
+      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+      cy.get('.search-keyword').type('ca')
+      cy.wait(2000)
+      cy.get('.products').as('productsLocator')
+      cy.get('@productsLocator').find('.product').each(($el) => {
+        const productText = $el.find('h4.product-name').text()
+        if(productText.includes("Cashews")) {
+          cy.wrap($el).find('button').click()
+        }
+      })
+      cy.get('.cart-icon > img').click()
+      cy.contains('PROCEED TO CHECKOUT').click()
+      cy.get('button').contains('Place Order').click()
+    })
+})
