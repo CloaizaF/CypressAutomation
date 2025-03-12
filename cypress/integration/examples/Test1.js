3f1a8016da85bb1f9e0f20e2a56a54dd40602fb7
CloaizaF <cloaiza@unal.edu.co>
2025-03-12 11:08:49 -0500

    Add promise to do a sequential console log

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -14,6 +14,10 @@ describe('My First Test Suite', () => {
       cy.get('@productsLocator').find('.product').should('have.length', 4)
 
       cy.get('@productsLocator').find('.product').eq(2).contains("ADD TO CART").click()
+        .then( () => {
+          console.log("Product was added to cart")
+          // Handling the promise manually as this is NON-Cypress command, it's JS code
+      })
       cy.get(':nth-child(3) > .product-action > button').click() //- not recommended
 
       cy.get('@productsLocator').find('.product').each(($el) => {
@@ -23,9 +27,12 @@ describe('My First Test Suite', () => {
         }
       })
 
+      // Assert if logo text is displayed correctly
+      cy.get('.brand').should('have.text', 'GREENKART')
+
       // I need to handle the promise manually when I want to save output in vars
       cy.get('.brand').then((logoElement) => { 
-        cy.log(logoElement.text()) 
+        cy.log(logoElement.text()) // To print in logs
       })
 
     })
