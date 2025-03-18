a38679d122f2f24ea9751f52a8d851bc848b4ddd
CloaizaF <cloaiza@unal.edu.co>
2025-03-18 11:32:05 -0500

    Add test to verify elements visbility

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -24,12 +24,24 @@ describe('Verify elements in webpage', () => {
       cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
 
       cy.get('#autocomplete').type('ind')
-
       cy.get('.ui-menu-item div').each(($el) => {
         if($el.text() === "India") {
           cy.wrap($el).click()
         }
       })
+      cy.get('#autocomplete').should('have.value', 'India')
+      
+    })
+
+    it('Verify elements visbility and invisibility', () => {
+
+      cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+      cy.get('#displayed-text').should('be.visible')
+      cy.get('#hide-textbox').click()
+      cy.get('#displayed-text').should('not.be.visible')
+      cy.get('#show-textbox').click()
+      cy.get('#displayed-text').should('be.visible')
       
     })
 })
