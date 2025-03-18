2e1566479d564eea73d1851a224dde4f5942d760
CloaizaF <cloaiza@unal.edu.co>
2025-03-18 18:37:45 -0500

    Add test to check table

---
+++ cypress/integration/examples/Test5.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+
+describe('Verify Table', () => {
+    it('Verify course has price', () => {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        cy.get('tr td:nth-child(2)').each(($el, $index) => {
+            const courseText = $el.text() 
+            if(courseText.includes("Python")) {
+                cy.get('tr td:nth-child(2)').eq($index).next().then(($price) => {
+                    const priceText = $price.text()
+                    expect(priceText).to.equal('25')
+                })
+            }
+        })
+
+    })
+})
