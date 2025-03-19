5fe688ffa7e4ec73203e95ac387220bb87b1e927
CloaizaF <cloaiza@unal.edu.co>
2025-03-19 15:07:11 -0500

    Update  test to work with frames

--- cypress/integration/examples/Test9.js
+++ cypress/integration/examples/Test9.js
@@ -7,6 +7,7 @@ describe('Handling Frames', () => {
 
         cy.frameLoaded('#courses-iframe')
         cy.iframe().find('a[href*="mentorship"]').eq(0).click()
+        cy.wait(3000)
         cy.iframe().find('h1[class*="pricing-title"]').should('have.length', 2)
 
         
