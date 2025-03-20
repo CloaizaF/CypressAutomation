03beac17103a39b818f91150c75937f949556f05
CloaizaF <cloaiza@unal.edu.co>
2025-03-20 11:13:12 -0500

    Add custom scripts

--- cypress.config.js
+++ cypress.config.js
@@ -4,15 +4,21 @@ module.exports = defineConfig({
 
   defaultCommandTimeout: 6000,
   reporter: 'cypress-mochawesome-reporter',
+  projectId: 'gk47fx',
 
   env: {
     url: "https://rahulshettyacademy.com"
   },
   
+  retries: {
+    runMode: 1,
+    openMode: 1,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/*/*.js'
   },
 });
