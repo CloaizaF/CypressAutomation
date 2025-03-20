9ef43efde922dde555bee0519384f8b8feeecbad
CloaizaF <cloaiza@unal.edu.co>
2025-03-20 10:04:46 -0500

    Add reports for test cases

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
 
   defaultCommandTimeout: 6000,
+  reporter: 'cypress-mochawesome-reporter',
 
   env: {
     url: "https://rahulshettyacademy.com"
@@ -10,7 +11,7 @@ module.exports = defineConfig({
   
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     specPattern: 'cypress/integration/examples/*.js'
   },
