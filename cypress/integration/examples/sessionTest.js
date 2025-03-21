884b377f29f3a563437bbf2a0215d9c1aa5c5506
CloaizaF <cloaiza@unal.edu.co>
2025-03-21 13:38:56 -0500

    Add csv dependency

--- cypress/integration/examples/sessionTest.js
+++ cypress/integration/examples/sessionTest.js
@@ -12,5 +12,18 @@ describe('Verify JWT Session', () => {
                 })
         })
 
+        cy.get('.card-body button:last-of-type').eq(1).click()
+        cy.get('[routerlink*="cart"]').click({force: true})
+        cy.contains("Checkout").click()
+        cy.get('input[placeholder*="Country"]').type("Ind")
+        cy.get('.ta-results button').each(($el) => {
+            if ($el.text() === " India") {
+                cy.wrap($el).click()
+            }
+        })
+        cy.get('.action__submit').click()
+        cy.wait(3000)
+        cy.get('.order-summary button').eq(0).click()
+
     })
 })
